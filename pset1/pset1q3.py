0f09551c0ede5c8000f71ec6ec07816d57c8642c
Ken Cheung <z419379295@gmail.com>
2017-09-06 21:55:52 +1000

    Added solution to question 3 of pset1.

--- pset1/pset1q3.py
+++ pset1/pset1q3.py
@@ -15,4 +15,24 @@ For example, if s = 'abcbcd', then your program should print
 Longest substring in alphabetical order is: abc
 """
 
-# TODO...
+def find_longest_substring_in_alphabetical_order(s):
+    groups = []
+    cur_longest = ''
+    prev_char = '' # this equals to 0
+    for i in range(0, len(s)):
+        if prev_char and s[i] < prev_char:
+            groups.append(cur_longest)
+            cur_longest = s[i]
+        else:
+            cur_longest += s[i] # concatenate
+        # if reached end of string, next if statement will not be executed.
+        # Add this guard statement to check in case the last n letters are longest
+        if (i == len(s)-1 and s[i] > prev_char):
+           groups.append(cur_longest)
+        prev_char = s[i]
+    return max(groups, key=len) if groups else s
+
+s = 'eycstpipqz' # test vector
+
+count = find_longest_substring_in_alphabetical_order(s)
+print ("Longest substring in alphabetical order is:", count)
