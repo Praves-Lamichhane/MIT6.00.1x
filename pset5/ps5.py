679b2dae49a4639b43f77170651a38645778f17f
Ken Zhang <ken.zhang028@gmail.com>
2018-06-17 17:00:30 +1000

    Added solution to pset5 problem 2.

--- pset5/ps5.py
+++ pset5/ps5.py
@@ -160,7 +160,11 @@ class PlaintextMessage(Message):
         Hint: consider using the parent class constructor so less 
         code is repeated
         '''
-        pass #delete this line and replace with your code here
+
+        Message.__init__(self, text)
+        self.shift = shift
+        self.encrypting_dict = Message.build_shift_dict(self, shift)
+        self.message_text_encrypted = Message.apply_shift(self, shift)
 
     def get_shift(self):
         '''
@@ -168,7 +172,8 @@ class PlaintextMessage(Message):
         
         Returns: self.shift
         '''
-        pass #delete this line and replace with your code here
+
+        return self.shift
 
     def get_encrypting_dict(self):
         '''
@@ -176,7 +181,11 @@ class PlaintextMessage(Message):
         
         Returns: a COPY of self.encrypting_dict
         '''
-        pass #delete this line and replace with your code here
+        # pass #delete this line and replace with your code here
+        org_dict = self.encrypting_dict
+        cpy_dict = dict(org_dict)
+
+        return cpy_dict
 
     def get_message_text_encrypted(self):
         '''
@@ -184,7 +193,8 @@ class PlaintextMessage(Message):
         
         Returns: self.message_text_encrypted
         '''
-        pass #delete this line and replace with your code here
+
+        return self.message_text_encrypted
 
     def change_shift(self, shift):
         '''
@@ -197,8 +207,10 @@ class PlaintextMessage(Message):
 
         Returns: nothing
         '''
-        pass #delete this line and replace with your code here
 
+        self.shift = shift
+        self.encrypting_dict = Message.build_shift_dict(self, shift)
+        self.message_text_encrypted = Message.apply_shift(self, shift)
 
 class CiphertextMessage(Message):
     def __init__(self, text):
